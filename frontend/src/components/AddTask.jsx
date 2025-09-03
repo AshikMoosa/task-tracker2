c4902cd8f166a3de14ecb42e014573c6d32ab82d
ashikmoosa <ashikmoosa96@gmail.com>
2025-09-03 19:45:32 +0100

    Code Cleanup for AddTask

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,11 +1,15 @@
-import { Input, Checkbox, Button, Flex } from "@chakra-ui/react";
+import { Field, Input, Checkbox, Button, Flex } from "@chakra-ui/react";
 
 const AddTask = () => {
   return (
     <form>
       <Flex gap="4" direction="column">
-        <Input placeholder="Enter your email" />
-        <Input placeholder="Enter your email" />
+        <Field.Root>
+          <Field.Label>Task</Field.Label>
+          <Input placeholder="Enter Task Name" />
+          <Field.Label>Day & Time</Field.Label>
+          <Input placeholder="Enter Day & Time" />
+        </Field.Root>
         <Checkbox.Root>
           <Checkbox.HiddenInput />
           <Checkbox.Control />
--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -3,7 +3,9 @@ import { Flex, Text, IconButton } from "@chakra-ui/react";
 import Task from "./Task.jsx";
 
 const Tasks = ({ task }) => {
-  if (!task || task.length === 0) return <Text>No tasks for today!!</Text>;
+  // task.length = 0;
+  if (!task || task.length === 0)
+    return <Text color="green.500">No tasks for today!!</Text>;
   return (
     <VStack align="normal">
       {task.map((item, index) => (
