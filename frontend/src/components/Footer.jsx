0abb842bd6ead52617b77fd74b9141fb01a7730e
ashikmoosa <ashikmoosa96@gmail.com>
2025-09-07 02:00:37 +0100

    Fixed navigation using Link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,20 @@
-import { Heading, Flex } from "@chakra-ui/react";
+import { Heading, Highlight, Flex } from "@chakra-ui/react";
+import { Link, useLocation } from "react-router";
+
 const Footer = () => {
+  let currentLocation = useLocation();
   return (
     <footer>
       <Flex direction="column" align="center">
         <Heading size="sm">Copyright &copy; 2025</Heading>
         <Heading size="sm">Created By Ashik Moosa</Heading>
+        <Heading size="sm">
+          {currentLocation.pathname === "/about" ? (
+            <Link to="/">Home</Link>
+          ) : (
+            <Link to="/about">About</Link>
+          )}
+        </Heading>
       </Flex>
     </footer>
   );
